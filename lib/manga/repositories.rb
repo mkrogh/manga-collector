ffb687ac8a79d6433db3e17a764f1ded29c41b00
Markus Krogh <markus@casadelkrogh.dk>
2012-01-12 21:10:29 +0100

    Added 1.8 support (require relative fix)

--- lib/manga/repositories.rb
+++ lib/manga/repositories.rb
@@ -2,6 +2,19 @@ require "bundler/setup"
 require "open-uri"
 require "fileutils"
 Bundler.require(:default)
+
+def require_relative(relative_feature)
+  c = caller.first
+  fail "Can't parse #{c}" unless c.rindex(/:\d+(:in `.*')?$/)
+  file = $`
+  if /\A\((.*)\)/ =~ file # eval, etc.
+    raise LoadError, "require_relative is called in #{$1}"
+  end
+  absolute = File.expand_path(relative_feature, File.dirname(file))
+  require absolute
+end unless Kernel.respond_to?(:require_relative)
+
+
 require_relative "models"
 
 module Manga
